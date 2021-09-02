82ec9c4dfa6c111208ccdab51120fc04419ebcd6
RidhaBouyahi <ridha.bouyahyi@outlook.com>
2021-09-02 14:22:36 +0100

    Delete

--- Scripts/Installing Packages and Testing/test_packages.py
+++
@@ -1,5 +0,0 @@
-import cv2
-import requests
-import numpy
-import pymongo
-import tflite_runtime.interpreter as tflite
--- Scripts/Installing Packages and Testing/testing_tf_packages.py
+++
@@ -1,3 +0,0 @@
-import tensorflow
-from tensorflow import keras
-import tflite_runtime.interpreter as tflite
